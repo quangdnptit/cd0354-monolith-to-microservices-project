a0402c5663a87b9de86ad387cc61240eb35accf4
quangdn4@fsoft.com <quangdn4@fsoft.com>
2022-06-23 00:58:47 +0700

    init code base

--- set_env.sh
+++ set_env.sh
@@ -1,10 +1,10 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
+export POSTGRES_USERNAME=udacity12345
+export POSTGRES_PASSWORD=udacity12345
+export POSTGRES_HOST=udacity12345.culna6sx4m9d.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
+export AWS_BUCKET=arn:aws:s3:::udacity12345
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
