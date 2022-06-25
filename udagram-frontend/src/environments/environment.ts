11f1808d06bbb969ba4e6f13890b4b4a1fbf1f7f
quangdn <quangdn@gemvietnam.com>
2022-06-26 02:18:12 +0700

    update fe version

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://aba1ca282b1cd4133afbbfd625a9203f-481609572.us-east-1.elb.amazonaws.com'
 };
 
 /*
